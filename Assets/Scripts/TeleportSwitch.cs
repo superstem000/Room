9d468ee3f0b843c78c38f80f8ac92cf4e2c5a100
superstem000 <bhpark1129@gmail.com>
2026-09-09 16:05:33 -0500

    teleportswitch update

--- Assets/Scripts/TeleportSwitch.cs
+++ Assets/Scripts/TeleportSwitch.cs
@@ -7,6 +7,9 @@ public class TeleportSwitch : MonoBehaviour
     public Transform rig;
     public Vector3 insidePosition = new Vector3(0f, 0f, 5f);
     public Vector3 outsidePosition = new Vector3(-40f, 0f, -15f);
+    public Vector3 insideRotation = Vector3.zero;
+    public Vector3 outsideRotation = new Vector3(0f, 75f, 0f);
+
 
     private bool outside = false;
 
@@ -17,6 +20,8 @@ public class TeleportSwitch : MonoBehaviour
         {
             outside = !outside;
             rig.position = outside ? outsidePosition : insidePosition;
+
+            rig.rotation = Quaternion.Euler(outside ? outsideRotation : insideRotation);
         };
     }
 }
